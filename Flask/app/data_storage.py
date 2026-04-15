819c9da147efd45a9b20be41d6afd8d795310e62
Michał <michalgarnis@gmail.com>
2026-04-15 10:14:50 +0200

    Add automatic port detection
    Now the server scans the ports in order to find connected arduino devices, and auto connects to the first one found

--- Flask/app/data_storage.py
+++ Flask/app/data_storage.py
@@ -1,6 +1,8 @@
 from threading import Lock
 import threading
-import serial
+
+from serial import Serial
+from serial.tools import list_ports
 import time
 
 # this class holds the information that were collected from arduino
@@ -26,11 +28,43 @@ class DataStorage:
 # creating a shared instance of the class
 data_store = DataStorage()
 
-# definition of a communication port with arduino
-ser = serial.Serial('COM6', 9600, timeout=1)
+
+def detect_arduino():
+    try:
+        ports = list_ports.comports()
+    except Exception as e:
+        raise RuntimeError(f"No arduino port found: {e}")
+
+    for port in ports:
+        desc = (port.description or "").lower()
+        hwid = (port.hwid or "").lower()
+
+        search_phrase = [
+            "arduino",
+            "ch340",
+            "wchusb",
+            "cdc",
+            "usb serial",
+            "mega",
+            "uno",
+            "nano"
+        ]
+
+        for phrase in search_phrase:
+            if (phrase in desc) or (phrase in hwid):
+                return port
+
+    raise RuntimeError("No arduino port found")
+
+
 
 # this function will be used by a separate thread to read data from the arduino
 def read_parameters():
+
+    # definition of a communication port with arduino
+    port = detect_arduino()
+    ser = Serial(port.device, 9600, timeout=1)
+
     while(True):
         command = 'GET\n'
         ser.write(command.encode())
